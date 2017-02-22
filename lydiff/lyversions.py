b316ef104955d053d4af682bd3c2cfe3849fa670
Urs Liska <git@ursliska.de>
2017-02-22 13:15:47 +0100

    Rename private members in Versions() class

--- lydiff/lyversions.py
+++ lydiff/lyversions.py
@@ -7,18 +7,17 @@ import subprocess
 
 class Versions:
     def __init__(self, paths, search=True):
-        self.paths = paths
-        self.list = []
+        self._list = []
         if ':' in paths:
-            self.paths = paths.split(':')
+            self._paths = paths.split(':')
         else:
-            self.paths = [paths]
+            self._paths = [paths]
         if search:
             self._executables()
 
     def _executables(self):
         """search for executables and fill internal list"""
-        pattern = ['%s/lilypond' % p for p in self.paths]
+        pattern = ['%s/lilypond' % p for p in self._paths]
         available_binaries = []
 
         for p in pattern:
@@ -30,8 +29,8 @@ class Versions:
         for binary in available_binaries:
             version = subprocess.check_output([binary, "--version"])
             version = [int(i) for i in version.split()[2].decode().split('.')]
-            self.list.append((*version, binary))
-        self.list.sort()
+            self._list.append((*version, binary))
+        self._list.sort()
 
     def _tuple(self, string):
         return tuple(int(i) for i in string.split("."))
@@ -41,36 +40,36 @@ class Versions:
 
     def __str__(self):
         string = ""
-        for version in self.list:
+        for version in self._list:
             string += "  %2d.%2d.%2d  %s\n" % version
         return string[:-1]
 
     @property
     def binaries(self):
-        return [v[3] for v in self.list]
+        return [v[3] for v in self._list]
 
     @property
     def versions(self):
-        #print(self.list[0][:3])
-        return [self._str(v[:3]) for v in self.list]
+        #print(self._list[0][:3])
+        return [self._str(v[:3]) for v in self._list]
 
     def get(self, version, similarfirst=True):
         """algo in 'next', 'previous', 'sameminor' """
-        #print(version, self.list)
+        #print(version, self._list)
         if version in self.versions:
             return version, self.binaries[self.versions.index(version)]
         if version == 'latest':
             return self.versions[-1], self.binaries[-1]
         # version not found, looking for best match according to algo
         v = self._tuple(version)
-        if v < self.list[0][:3]:
+        if v < self._list[0][:3]:
             print("Warning: required version %s is older than any installed version" % version)
             return self.versions[0], self.binaries[0]
-        if v > self.list[-1][:3]:
+        if v > self._list[-1][:3]:
             print("Warning: required version %s is newer than any installed version" % version)
             return self.versions[-1], self.binaries[-1]
         if similarfirst:  # TODO: not implemented
-            shortlist = [entry for entry in self.list if entry[:2] == v[:2]]
+            shortlist = [entry for entry in self._list if entry[:2] == v[:2]]
             for i, available in enumerate(shortlist):
                 if available[:3] > v:
                     break
@@ -78,7 +77,7 @@ class Versions:
                   version, self.versions[i]))
             return self.versions[i], self.binaries[i]
 
-        for i, available in enumerate(self.list):
+        for i, available in enumerate(self._list):
             if available[:3] > v:
                 break
         print("Warning: required version %s is not installed. Using %s instead." % (
